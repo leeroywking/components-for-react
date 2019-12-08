553b1c7f062ead5e471f280eb032fe8f80e97e47
Lee-Roy King <leeroywking@gmail.com>
2019-12-08 14:44:55 -0800

    Autocomplete is populating on screen

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AutoComplete from './components/autocomplete/autocomplete.js'
+
 
 function App() {
   return (
@@ -19,6 +21,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <AutoComplete/>
     </div>
   );
 }
---
+++ src/components/autocomplete/autocomplete.js
@@ -0,0 +1,30 @@
+import React, {useEffect, useState} from 'react';
+
+
+export default () => {
+  const [suggestions, setSuggestions] = useState([]);
+  
+  const handleSuggestions = async (event) => {
+    const URL = `https://oapi.herokuapp.com/autocomplete`
+    const limit = 20
+    const wordArr = event.target.value.split(' ')
+    const partial = wordArr[wordArr.length -1]
+    const response = await fetch(`${URL}/${partial}/${limit}`);
+    const responseObj = await response.json();
+    setSuggestions(responseObj.wordList)
+  }
+  return (
+    <form>
+      <ul>
+        {(suggestions.map(suggestion => (
+          <li>{suggestion}</li>
+        )))}
+      </ul>
+      <label>
+        Name:
+    <input type="text" name="name" onChange={handleSuggestions}/>
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+  )
+}
